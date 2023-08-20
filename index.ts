092c367834284461e6a66996320be8eb3adee218
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-20 23:27:22 +0300

    fin

--- index.ts
+++ index.ts
@@ -2,12 +2,15 @@ console.clear();
 import { from } from 'rxjs';
 import { distinct } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/distinct
+
 const obj1 = { id: 3, name: 'name 1' };
 const obj2 = { id: 4, name: 'name 2' };
 const obj3 = { id: 3, name: 'name 3' };
-const vals = [obj1, obj2, obj3];
+const arrOfObject = [obj1, obj2, obj3];
 
-from(vals)
+// filtering on id
+from(arrOfObject)
   .pipe(distinct((e) => e.id))
   .subscribe(console.log);
 
