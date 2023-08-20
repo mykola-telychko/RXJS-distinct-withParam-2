09cb04fb28f36e9263f88678d0937a865072a77a
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-20 23:22:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RXJS-distinct-withParam-2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-distinct-example-fbfvae)
---
+++ index.ts
@@ -0,0 +1,18 @@
+console.clear();
+import { from } from 'rxjs';
+import { distinct } from 'rxjs/operators';
+
+const obj1 = { id: 3, name: 'name 1' };
+const obj2 = { id: 4, name: 'name 2' };
+const obj3 = { id: 3, name: 'name 3' };
+const vals = [obj1, obj2, obj3];
+
+from(vals)
+  .pipe(distinct((e) => e.id))
+  .subscribe(console.log);
+
+/*
+OUTPUT:
+{id: 3, name: "name 1"}
+{id: 4, name: "name 2"}
+ */
